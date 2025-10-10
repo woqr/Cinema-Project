2e8fe6baa4b0eb6a7053a947abaae8668042da99
woqr <149923278+woqr@users.noreply.github.com>
2025-10-10 12:31:38 +0500

    Сайт готов

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-total");
+const menuButton=document.querySelector(".m-menu");
+const menu=document.querySelector(".menu");
 let cost = 800;
 let totalPrice = 0;
 schemeSvg.addEventListener("click", (event) => {
@@ -10,3 +12,7 @@ schemeSvg.addEventListener("click", (event) => {
     totalPriceTag.textContent = totalPrice;
   }
 });
+
+menuButton.addEventListener('click',() => {
+  menu.classList.toggle("is-open")
+})
